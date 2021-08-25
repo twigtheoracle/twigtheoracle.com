13793d8c7c5cdcad32210133d2fd8dedc1314d59
Eric Liu <twigoracle@gmail.com>
2021-08-24 17:33:07 -0700

    Table fill from file structure works, table sorting works

--- website/mysite/views.py
+++ website/mysite/views.py
@@ -15,7 +15,9 @@ sys.path.insert(1, str(settings.SD_DIR))
 from run import main
 import os
 
-# Create your views here.
+# thoughts page stuff
+from pathlib import Path
+from bs4 import BeautifulSoup
 
 def home(request):
     # the home page
@@ -101,7 +103,42 @@ def stock_data(request):
 def thoughts(request):
     # thoughts page
 
-    return render(request, "thoughts.html")
+    def get_dates(file_path):
+        """
+        The get_dates function takes the input html file path and uses beautiful soup to extract 
+        and return the metadata date of creation and date of modification.
+
+        :param:     file_path   A pathlib.Path object containing the path to the html file
+
+        :return:    str         A string in format "YYYY-MM-DD" representing the date of creation 
+        :return:    str         Same as above but date of modification
+        """
+        # use beautiful soup to get the creation date and modification date
+        with open(file_path) as f:
+            soup = BeautifulSoup(f, "html.parser")
+            creation_date = soup.find("meta", {"name": "creation-date"})["content"]
+            modification_date = soup.find("meta", {"name": "modification-date"})["content"]
+
+            # return those dates
+            return [creation_date, modification_date]
+
+    # create the list of thoughts
+
+    # the directory that thoughts are stored in 
+    thoughts_dir = settings.BASE_DIR / "mysite" / "templates" / "thoughts"
+
+    # find all html files in the thoughts dir
+    html_files = list(thoughts_dir.glob("**/*.html"))
+
+    # store the thoughts list here
+    thought_list = []
+
+    # for each html file, get the creation date and modification date
+    for html_file in html_files:
+        creation_date, modification_date = get_dates(thoughts_dir / html_file)
+        thought_list.append([creation_date, modification_date, html_file.stem])
+
+    return render(request, "thoughts.html", {"thought_list": thought_list})
 
 def specific_thought(request, **kwargs):
 
