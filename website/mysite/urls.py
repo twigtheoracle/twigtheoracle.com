6e6d8f8e69a06c5e68e0e04bd3c2874995e9bdbb
Eric Liu <10490357+twigtheoracle@users.noreply.github.com>
2021-08-24 17:52:07 -0700

    Thoughts page (#12)
    * Added placeholder page
    
    * Added jupyter notebook styling to css
    
    * Working nb convert with example notebook
    
    Need to work on updating metadata
    
    * Fixed center aligned content -> left aligned
    
    * Metadata update should work
    
    * Table fill from file structure works, table sorting works
    
    * Finished thought page, everything should work

--- website/mysite/urls.py
+++ website/mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about", views.about, name="about"),
     path("projects", views.projects, name="projects"),
-    path("projects/stock_data", views.stock_data, name="stock_data")
+    path("projects/stock_data", views.stock_data, name="stock_data"),
+    path("thoughts", views.thoughts, name="thoughts"),
+    path("thoughts/<thought_name>", views.specific_thought, name="specific_thought")
 ]
